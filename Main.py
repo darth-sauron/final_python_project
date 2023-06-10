a63a6d8bc15902d4fe2a4f96fbc0590e4ff9c978
darth-sauron <95109048+darth-sauron@users.noreply.github.com>
2023-06-10 21:57:32 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,8 +4,8 @@ import GUIpart
 # races = "https://www.wargamer.com/dnd/races"
 # classes = "https://www.wargamer.com/dnd/classes"
 #
-# j_races = "C:\\Users\\darya\\PycharmProjects\\pythonProject\\Final Project\\races.json"
-# j_classes = "C:\\Users\\darya\\PycharmProjects\\pythonProject\\Final Project\\classes.json"
+# j_races = "races.json"
+# j_classes = "classes.json"
 # WebsiteScrapingPart.make_races(races, j_races)
 # WebsiteScrapingPart.make_races(classes, j_classes)
 # DataBasePart.populate_races()
